16076bd3ae55833a27d43c59ea7ad711fc40beb5
ritz-w <ritz.wu@outlook.com>
2018-08-28 13:14:59 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  const new_driver = {...driver};
+  new_driver[key] = value;
+  return new_driver;
+}
+
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+}
+
+
+function deleteFromDriverByKey(driver, key) {
+  const new_driver = {...driver};
+  delete new_driver[key];
+  return new_driver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
